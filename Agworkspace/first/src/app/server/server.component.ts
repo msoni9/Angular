1904f79b6378f62c710effe362d9bea9e5d41369
Mayank Soni <mayank.soni@yash.com>
2019-12-12 16:34:27 +0530

    Angular assignment

---
+++ Agworkspace/first/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.Component.html',
+})
+export class ServerComponent{
+
+    serverId = 10;
+    serverStatus = 'offline'
+
+}
---
+++ Agworkspace/project/src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.Component.html'
+})
+export class HeaderComponent{
+
+   @Output() featureSelected = new EventEmitter<string>();
+
+    onSelect(feature: string){
+
+        this.featureSelected.emit(feature);
+
+    }
+
+}
